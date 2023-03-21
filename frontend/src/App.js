1d32f07a4fda5e59d732ae67d33c8128c1272a5a
nesb01t <nesb01t@hotmail.com>
2023-03-22 01:04:25 +0800

    Update 增加background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import background from './background.webp';
 import BkgIcon from './component/BkgIcon';
 
 function App() {
   return (
     <div className="App">
+
       <BkgIcon></BkgIcon>
       Learn React
+      <img className="Background" src={background} alt="bkg"></img>
     </div>
   );
 }
+
 
 export default App;
