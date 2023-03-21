33c9b671dd2935f5b913e162995fcfb9ae95ff50
nesb01t <nesb01t@hotmail.com>
2023-03-22 00:33:38 +0800

    Update 初始化前端

---
+++ frontend/src/component/BkgIcon/index.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+export default function BkgIcon() {
+    return (
+        <div className="BkgIcon">
+            <img src={logo} className="App-logo" alt="logo" />
+            <button>Click me</button>
+        </div>
+    );
+}
