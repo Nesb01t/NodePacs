5b78689395305b03ded38f6d24d7227bd1052739
Nesb01t <nesb01t@hotmail.com>
2023-04-21 02:48:47 +0800

    Update 后端框架完成

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const fs = require('fs');
+const local = require('./local.js');
 
 const app = new Koa();
 const logMiddleware = require('./middleware/log.js');
@@ -7,5 +8,5 @@ const dicomMiddleware = require('./middleware/dicom.js');
 
 // app.use(logMiddleware);
 app.use(dicomMiddleware);
-
 app.listen(3000);
+local()
